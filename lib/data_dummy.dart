a4ad1061936edc7d666e2f138e5f658ade398540
Distrotion <sittichok.ho@gmail.com>
2022-02-16 12:36:17 +0700

    .

---
+++ lib/data_dummy.dart
@@ -0,0 +1,40 @@
+// class
+
+class dataset {
+  dataset(this.id, this.f01, this.f02, this.f03, this.f04, this.f05, this.f06,
+      this.f07, this.f08, this.f09, this.f10);
+  final int id;
+  final String f01;
+  final String f02;
+  final String f03;
+  final String f04;
+  final String f05;
+  final String f06;
+  final String f07;
+  final String f08;
+  final String f09;
+  final String f10;
+
+  bool selected = false;
+}
+
+List<dataset> data_test = <dataset>[
+  dataset(1, 'taest', 'taest', 'daest', 'taest', 'taest', 'taest', 'taest',
+      'taest', 'taest', 'taest'),
+  dataset(2, 'tesasst', 'tesasst', 'cesasst', 'tesasst', 'tesasst', 'tesasst',
+      'tesasst', 'tesasst', 'tesasst', 'tesasst'),
+  dataset(3, 'astest', 'astest', 'fstest', 'astest', 'astest', 'astest',
+      'astest', 'astest', 'astest', 'astest'),
+  dataset(4, 'retest', 'retest', 'aetest', 'retest', 'retest', 'retest',
+      'retest', 'retest', 'retest', 'retest'),
+  dataset(5, 'fdsfstest', 'fdsfstest', 'bdsfstest', 'fdsfstest', 'fdsfstest',
+      'fdsfstest', 'fdsfstest', 'fdsfstest', 'fdsfstest', 'fdsfstest'),
+  dataset(6, 'eretest', 'eretest', 'gretest', 'eretest', 'eretest', 'eretest',
+      'eretest', 'eretest', 'eretest', 'eretest'),
+  dataset(7, 'rytest', 'rytest', 'cytest', 'rytest', 'rytest', 'rytest',
+      'rytest', 'rytest', 'rytest', 'rytest'),
+  dataset(8, 'rtrtest', 'rtrtest', 'ltrtest', 'rtrtest', 'rtrtest', 'rtrtest',
+      'rtrtest', 'rtrtest', 'rtrtest', 'rtrtest'),
+];
+
+// List<Dessert> data_test = [];
